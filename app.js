f96ae41ee33de4e21f9622bed3ec4a86ae7983e8
DivYesh Patel <pateldivyesh009@gmail.com>
2018-11-19 03:43:43 +0530

    SPA ready with Module, Routes, Controllers and Pages

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-//Module
+// Module
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+
+// Routes
+weatherApp.config(function ($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'homeCtrl'
+        })
+        .when('/forecast', {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastCtrl'
+        });
+});
+
+// Controllers
+weatherApp.controller('homeCtrl', ['$scope', function ($scope) {
+
+}]);
+
+weatherApp.controller('forecastCtrl', ['$scope', function ($scope) {
+
+}]);
